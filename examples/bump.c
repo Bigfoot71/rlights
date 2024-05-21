1fa89bb7f30d5ed225299e4377dbd2d821391d00
Bigfoot71 <victor.lejuez@protonmail.ch>
2024-05-21 14:16:43 +0200

    add bump mapping example

---
+++ examples/bump.c
@@ -0,0 +1,84 @@
+#include "raylib.h"
+#include "raymath.h"
+
+#define RLIGHTS_IMPLEMENTATION
+#include "../rlights.h"
+
+int main(void)
+{
+    // Initialization
+    InitWindow(800, 600, "bump mapping");
+
+    Camera camera = { 0 };
+    camera.position = (Vector3){ 1.2f, 0.4f, 1.2f };
+    camera.target = (Vector3){ 0.185f, 0.4f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    // Generate model
+    Image imMap = LoadImage("resources/cubicmap.png");
+    Texture2D cubicmap = LoadTextureFromImage(imMap);
+    Mesh mesh = GenMeshCubicmap(imMap, (Vector3){ 1.0f, 1.0f, 1.0f });
+    Model model = LoadModelFromMesh(mesh);
+    GenMeshTangents(&model.meshes[0]);
+    UnloadImage(imMap);
+
+    // Init light system
+    RLG_Init(1);
+    RLG_SetSpecular(0.2f);
+    RLG_EnableNormalMap();
+
+    RLG_EnableLight(0);
+    RLG_SetLightInnerCutOff(0, 17.5f);
+    RLG_SetLightOuterCutOff(0, 27.5f);
+    RLG_SetLightType(0, RLG_SPOTLIGHT);
+
+    // Set lighting shader to the model
+    model.materials[0].shader = *RLG_GetShader();
+
+    // Load and set textures for the model
+    Texture2D diffuse = LoadTexture("resources/atlas_albedo.png");
+    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = diffuse;
+
+    Texture2D normal = LoadTexture("resources/atlas_normal.png");
+    model.materials[0].maps[MATERIAL_MAP_NORMAL].texture = normal;
+
+    DisableCursor();    // Limit cursor to relative movement inside the window
+    SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
+
+    // Main game loop
+    while (!WindowShouldClose())
+    {
+        // Update
+        UpdateCamera(&camera, CAMERA_FREE);
+        RLG_SetViewPositionV(camera.position);
+
+        RLG_SetLightPositionV(0, camera.position);
+        RLG_SetLightTargetV(0, camera.target);
+
+        // Draw
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+                DrawModel(model, Vector3Zero(), 1.0f, WHITE);
+            EndMode3D();
+
+            DrawFPS(10, 10);
+
+        EndDrawing();
+    }
+
+    // De-Initialization
+    UnloadTexture(cubicmap);
+    UnloadTexture(diffuse);
+    UnloadTexture(normal);
+    UnloadModel(model);
+
+    RLG_Close();
+    CloseWindow();
+
+    return 0;
+}
